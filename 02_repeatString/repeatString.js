656dc0ebe0d7e19dec40e838c40f3467bedec4ec
eustaquio <eustaquio.galano@gmail.com>
2024-11-12 15:09:56 +0800

    Solve repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(wordToRepeat, repeatCount) {
 
+    repeatedWord = '';
+
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < repeatCount; i++) {
+        
+        repeatedWord += `${wordToRepeat}`;
+    }
+
+    return  repeatedWord
 };
+
+// repeatString('hey', 3);
+// repeatString('hello', 10);
+// repeatString('hi', 1);
+// repeatString('bye', 0);
+
 
 // Do not edit below this line
 module.exports = repeatString;
