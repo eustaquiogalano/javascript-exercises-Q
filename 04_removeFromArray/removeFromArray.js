76c3e1d7e49eb035ba00b48ff7a4a60247417546
eustaquio <eustaquio.galano@gmail.com>
2024-11-12 21:36:49 +0800

    Solve remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+
+    let newList = arr.filter((element) => {
+        if (valuesToRemove.includes(element)) {
+            return false;
+        }
+        return true;
+    });
+
+    return newList;
 };
 
 // Do not edit below this line
