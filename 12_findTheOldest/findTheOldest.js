275b3af2bd4b7801a8a54640da4c7f50d04352c2
eustaquio <eustaquio.galano@gmail.com>
2024-11-15 23:49:55 +0800

    Solve get the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(listOfPeople) {
+
+    const oldest = listOfPeople.reduce((accumulator, currentPerson) => {
+        const oldestAge = getAge(accumulator.yearOfBirth, accumulator.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return currentAge > oldestAge ? currentPerson : accumulator; 
+    });
+   return oldest;
 };
 
 // Do not edit below this line
