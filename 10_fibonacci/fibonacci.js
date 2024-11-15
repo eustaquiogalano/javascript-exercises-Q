dceaee566dee0a4be98b1f3ad63dc4a7e6003b60
eustaquio <eustaquio.galano@gmail.com>
2024-11-15 14:10:33 +0800

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciNumber) {
 
+    let verifiedNumber;
+
+    if (typeof fibonacciNumber === 'string') {
+       verifiedNumber = parseInt(fibonacciNumber);
+    } else {
+        verifiedNumber = fibonacciNumber;
+    }  
+
+
+    if (verifiedNumber < 0) return "OOPS";
+    if (verifiedNumber === 0) return 0; 
+
+    let previous = 1;
+    let present = 0;
+    let result = 0;
+
+    for (let i = 0; i < verifiedNumber; i++) {
+        
+        result = present + previous;
+        previous = present;
+        present = result;
+
+    }
+
+    return result;
+    
 };
 
 // Do not edit below this line
